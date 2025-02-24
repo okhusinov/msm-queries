9678ff110ea9777e4e3914f02e772568aaa1452f
okhusinov <okhusinov@chicagobooth.edu>
2025-02-24 00:11:00 +0000

    finalssssssssssss

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -3,6 +3,16 @@ class DirectorsController < ApplicationController
     render({ :template => "director_templates/list" })
   end
 
+  def youngest
+    @youngest_director = Director.where.not({ :dob => nil }).order({ :dob => :desc }).at(0)  
+    render({ :template => "director_templates/youngest" })
+  end
+
+  def eldest
+    @eldest_director = Director.where.not({ :dob => nil }).order({ :dob => :asc }).at(0)  
+    render({ :template => "director_templates/eldest" })
+  end
+
   def show
     the_id = params.fetch("the_id")
     matching_records = Director.where({ :id => the_id })
